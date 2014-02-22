a834f9cec9a5f98a796242c653f52578e6a2fecc
Yasyf Mohamedali <yasyf@yasyf.com>
2014-02-22 17:37:27 -0500

    draggable numbers

--- McHacks.js
+++ McHacks.js
@@ -1,17 +1,72 @@
+Sessions = new Meteor.Collection("sessions");
 Inputs = new Meteor.Collection("inputs");
+Positions = new Meteor.Collection("positions");
 if (Meteor.isClient) {
-  Template.inputs.items = function (){
-    return Inputs.find({}).fetch();
+  var gridster;
+  Meteor.startup(function () {
+    if(location.hash.length < 2){
+      location.hash = Math.random().toString(36).substring(2,7);
+    }
+    Session.set("session",location.hash.substring(1));
+  });
+
+  Template.inputs.rendered = function() {
+    if(!gridster){
+      gridster = $(".gridster ul").gridster({
+          widget_margins: [10, 10],
+          widget_base_dimensions: [40, 40],
+          draggable: {
+            stop: save_pos
+          },
+          serialize_params: function($w, wgd){ 
+              return { 
+                     id: $($w).attr('id'), 
+                     col: wgd.col, 
+                     row: wgd.row, 
+                     size_x: wgd.size_x, 
+                     size_y: wgd.size_y, 
+                     content: $($w).html() 
+                   };
+          }
+      }).data('gridster');
+    }
+  }
+
+  Deps.autorun(function () {
+    pos = Positions.findOne({session: Session.get("session")});
+    if(gridster && Session.get('session')){
+      gridster.remove_all_widgets();
+      pos.diff.forEach(function(e){
+        gridster.add_widget('<li class="item number">'+e.content+'</li>',1,1,e.col,e.row);
+      });
+    }
+  });
+
+  function save_pos() {
+    current_pos =  gridster.serialize();
+    id = Positions.findOne({session: Session.get("session")});
+    if(id){
+      Positions.update({_id: id._id},{$set: {diff: current_pos}});
+    }
+    else{
+      Positions.insert({diff: current_pos, session: Session.get("session")});
+    }
   }
 
   Template.top.events({
-    'click #addbtn' : function () {
-      Inputs.insert({i: $('#in').val()});
+    'submit #addfrm' : function (e) {
+      e.preventDefault();
+      val = $('#i').val();
+      Inputs.insert({i: val, session: Session.get("session")});
+      gridster.add_widget('<li class="item number">'+val+'</li>');
+      save_pos();
+      $('#i').val('');
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    //Inputs.remove({});
   });
 }
